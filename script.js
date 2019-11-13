1a49070cbcddc9ec36b3ba1dab90820c3cd50d97
ostapskolozdra <69ostap18s@gmail.com>
2019-11-13 21:07:02 +0200

    add 2 files

---
+++ script.js
@@ -0,0 +1,112 @@
+// змінні
+var myNumber = 10.484
+var myString = 'Hello World'; // '10.484'
+var myBoolaen = true; // false
+var myNull = null;
+var myUndefined = undefined;
+
+
+
+
+/*console.log(40 + myNumber);
+console.log(40 - myNumber);
+console.log(40 * myNumber);
+console.log(40 / myNumber);
+myNumber--;
+console.log(myNumber);
+console.log(Math.round(5.5));
+
+var newNumber = 2.597;
+console.log(newNumber.toFixed(2));*/
+
+
+
+// Рядки
+
+/*console.log('40' + myNumber);
+console.log('Hello world!' + ' how are you?');
+console.log(myString.toUpperCase());*/
+
+
+
+// Масиви
+
+var names = ['Вася', 'Петя', 'Женя'];
+/*console.log(names[1].toUpperCase());
+
+names[0] = 'Маша';
+console.log(names[0]);
+
+names.push('Світлана');
+console.log(names);
+alert(names);*/
+
+
+
+//Умови
+
+/*if ('Вася' !== 'Вася' || myNumber === 10.484 ) {
+	console.log('Умовa виконалась');
+}
+
+if (myNumber < 200) {
+	console.log('Число більше 200');
+} else {
+	console.log('Число менше 200');
+}*/
+
+
+// Цикли
+
+/*for (var i = 0; i <= 15; i++) {
+	if(i==7){
+	continue;
+	}
+	console.log(i);
+}
+
+for (let j = 0; j < names.length; j++) {
+	console.log(names[j]);
+}*/
+
+
+/*let i = 0;
+while (i < 10) {
+	console.log(i);
+	i++;
+}*/
+
+
+
+// Функції
+
+/*function sum(x, y,) {
+	return x + y;
+}
+console.log(sum(10, myNumber));
+console.log(sum(464, myNumber));*/
+
+
+//Об'єкти
+
+/*var myObject = {
+	name: 'Vasia',
+	surname: 'Petrovich ',
+	age: 25,
+	getFullname: function() {
+		return this.name + ' ' + this.surname;
+	}
+};
+
+console.log(myObject.age);
+console.log(myObject.getFullname());*/
+
+
+
+let numbers = [22, 5, 6, 11, 9, 3, 12, 78, 45];
+    for (let i = 0; i < numbers.length; i++) {
+		console.log(numbers[i])
+	}
+
+
+
